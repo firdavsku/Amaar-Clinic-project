0464253cdfb72a4403c34e5112b1d563f792b1f5
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-11-23 11:52:36 +0500

    [ADDED] csrf token

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@zu-_22d&0^d)0c82!5!sy!#w9a0chlg8)ai@u%)k)tx3x7xya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://alamis.kz", "https://amaarclinic.pythonanywhere.com"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://alamis.kz',
+]
+CSRF_COOKIE_SECURE = True
+
 
 
 # Application definition
